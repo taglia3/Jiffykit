d584325e9092aa87adbb40697c142ab9c6d7565d
Matteo Tagliafico <m.tagliafico@reply.it>
2017-04-05 12:03:25 +0200

    Initial Commit

---
+++ JiffyKit/JiffyKit.h
@@ -0,0 +1,19 @@
+//
+//  JiffyKit.h
+//  JiffyKit
+//
+//  Created by Matteo Tagliafico on 05/04/2017.
+//  Copyright © 2017 Matteo Tagliafico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JiffyKit.
+FOUNDATION_EXPORT double JiffyKitVersionNumber;
+
+//! Project version string for JiffyKit.
+FOUNDATION_EXPORT const unsigned char JiffyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JiffyKit/PublicHeader.h>
+
+
